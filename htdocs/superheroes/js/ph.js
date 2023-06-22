2c0b0c1e8ee59e17d3f16b2f92fd3f98048bf8d7
Katerina <yusie@live.cn>
2023-06-21 19:57:52 -0600

    add placeholders to js folders

---
+++ htdocs/superheroes/js/ph.js
@@ -0,0 +1,4 @@
+// all projects are commited on github and deplyed by AWS amplify
+// as github won't store empty folder
+// this is just a placeholder file so that the js folder will show in the folder structure
+// for assignment purpose
